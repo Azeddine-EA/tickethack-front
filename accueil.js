2f185759176617436086347d3051ca42bda48e7b
Aïssatou <aissa.ndiaye@outlook.com>
2025-11-05 15:47:45 +0100

    Frontend: création de la vue No trip found et gestion du bouton Book à orienter vers la page panier

--- accueil.js
+++ accueil.js
@@ -4,18 +4,52 @@ const inputArrival = document.querySelector("#arrival-form")
 const inputDate = document.querySelector("#date-form")
 
 
+function allBtnBook(){
+    let btnBooks = document.querySelectorAll('.btnBook > button')
+    console.log(btnBooks)
+    for (const btnBook of btnBooks) {
+        
+        btnBook.addEventListener("click",function(){
+            
+            fetch(`http://localhost:3000/panier/${btnBook.id}`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                // body: JSON.stringify({ cityName }),
+            }).then(response=>response.json())
+            .then(panierData=>{
+                console.log(panierData)
+            })
+        })
+    }
+    return btnBooks
+}
+
 
 btnSearch.addEventListener('click',
     function () {
+        const containerTrajet = document.querySelector("#afficheTrips")
+        containerTrajet.style.display="none";
         const resulttrue = document.querySelector("#trips")
+        resulttrue.style.display="flex";
          resulttrue.innerHTML=''
         console.log('Click detected')
 fetch(`http://localhost:3000/trips/${inputDeparture.value}/${inputArrival.value}/${inputDate.value}`)
 .then(response => response.json())
 .then(tripsData =>{
-console.log(tripsData.Trips)
-
-    for (const trip of tripsData.Trips) {
+    const vide = []
+    console.log(tripsData);
+    if (tripsData.Trips.length === 0) {
+        resulttrue.style.display="none";
+        const notFound = document.querySelector("#resultfalse")
+        notFound.style.display="flex"
+        console.log('vide');
+        
+    }else{
+        // console.log('trouve');
+        const notFound = document.querySelector("#resultfalse")
+        notFound.style.display="none";
+        // console.log(tripsData.Trips)
+        for (const trip of tripsData.Trips) {
         // console.log(trip.departure)
         resulttrue.innerHTML+=`
                     <div class="trip">
@@ -29,22 +63,26 @@ console.log(tripsData.Trips)
                             <p>${trip.price}$</p>
                         </div>
                         <div class="btnBook">
-                            <button>Book</button>
+                            <button id="${trip._id}"><a href ="./cart.html">Book</a></button>
                         </div>
                     </div>
          
         `
+        }
+
+        inputDeparture.value = ''
+        inputArrival.value = ''
+        inputDate.value = ''
+        allBtnBook()
     }
-})
-inputDeparture.value = ''
-inputArrival.value = ''
-inputDate.value = ''
+    })
+
+
+
+
     }
-
-
-
+    
 )
-
 
 
 // btnSearch.addEventListener('click', function(){
