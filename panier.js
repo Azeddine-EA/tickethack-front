59d8fdafba885f457fec3622e2f3827b8be4c50d
fateh <kheloufifateh75@gmail.com>
2025-11-05 13:08:57 +0100

    ajt du fetch panier coté front pour afficher tout le panier

--- panier.js
+++ panier.js
@@ -1,31 +1,42 @@
-const cart = [
-  { depart: "Toulouse", arrivee: "Lyon", heure: "20:09", prix: 103 },
-  { depart: "Lyon", arrivee: "Paris", heure: "13:09", prix: 103 },
-];
-
 const cartItems = document.getElementById("cart-items");
 const totalEl = document.getElementById("total");
 
 function afficherPanier() {
   cartItems.innerHTML = "";
+  fetch("http://localhost:3000/panier")
+    .then((Response) => Response.json())
+    .then((panierData) => {
+      //document.querySelector('#purchase').textContent = Data.value;
+      console.log("ma data", panierData.Panier);
+      let totalPrice = 0
+      for (const element of panierData.Panier) {
+        // console.log(element.idTrip.departure);
+        // console.log(element.idTrip.arrival);
+        // div.classList.add("panierData-item");
+        cartItems.innerHTML += `
+        <div class="cart-item">
+            <span>${element.idTrip.departure} => ${element.idTrip.arrival}</span>
+            <span>${element.idTrip.date.slice(11,16)}</span>
+            <span>${element.idTrip.price}€</span>
+            <button class="${element._id}" >X</button>
+        </div>
+      `;
+        totalPrice += element.idTrip.price
+      totalEl.textContent = `Total : ${totalPrice}€`;
+      }
+    //   const div = document.createElement("div");
+      //         cartItems.appendChild(div);
+      //         total += trajet.prix;
+      //       }
 
-  let total = 0;
-  for (let i = 0; i < cart.length; i++) {
-    const trajet = cart[i];
-    const div = document.createElement("div");
-    div.classList.add("cart-item");
-    div.innerHTML = `
-    <span>${trajet.depart} => ${trajet.arrivee}</span>
-    <span>${trajet.heure}</span>
-    <span>${trajet.prix}€</span>
-    <button onclick="supprimerTrajet(${i})">X</button>
-  `;
-    cartItems.appendChild(div);
-    total += trajet.prix;
-  }
-
-  totalEl.textContent = `Total : ${total}€`;
+      // });
+    });
 }
+//   let total = 0;
+
+//   document.querySelector("#purchase").textContent = `Total : ${total}€`;
+// }
+//.catch((err) => console.error("Erreur lors du chargement du panier :", err));
 
 function supprimerTrajet(index) {
   cart.splice(index, 1);
